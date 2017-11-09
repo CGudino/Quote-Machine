0bf814fed0dadd1632d0ccfe66d59dbcc3ee877a
Cristian Gudiño <CGudino1359@gmail.com>
2017-11-09 00:54:21 -0800

    Cleaned up JS

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
+  function newBackground() {
+    var colors = ['red', 'blue', 'gray', 'pink', 'purple', 'yellow'];
+    var choice = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[choice];
+  }
+  
   $('#get-quote').on('click', function() {
     $.getJSON('https://talaikis.com/api/quotes/random/', function(json) {
-      var html = '';
       
-      html += "<p id='random-quote'><strong>'";
-      html += json.quote;
-      html += "'</strong></p>";
+      // Adds quote and author to page
+      $('#quote-box').html(`<p id='random-quote'><strong>${json.quote}</strong></p><p id='quote-author'><em>-${json.author}</em></p>`);
       
-      html += "<p id='quote-author'><em>-";
-      html += json.author;
-      html += "</em></p>";
-      
-      $('#quote-box').html(html);
-      $('#tweet-link').attr('href', 'https://twitter.com/intent/tweet?text=' + json.quote + ' -' + json.author);
+      // Button to tweet the current quote
+      $('#tweet-link').attr('href', `https://twitter.com/intent/tweet?text=${json.quote} -${json.author}`);
+      newBackground();
       });
    });
 });
